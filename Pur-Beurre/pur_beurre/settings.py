84c0071c67fb4137d552afaf48465431af3f54e1
toto <toto@gmail.com>
2020-05-07 11:22:46 +0200

    another round

--- Pur-Beurre/pur_beurre/settings.py
+++ Pur-Beurre/pur_beurre/settings.py
@@ -139,7 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
