9ad968fdf2a3529f10d040ce2d5b9909b5db6b53
toto <toto@gmail.com>
2020-05-07 11:28:18 +0200

    another round2

--- Pur-Beurre/pur_beurre/settings.py
+++ Pur-Beurre/pur_beurre/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
